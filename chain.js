0602237f5a011960f9afde0217f36d8e3c8961df
H <68972646+hnfdm@users.noreply.github.com>
2025-04-28 22:23:58 +0700

    Create chain.js

---
+++ chain.js
@@ -0,0 +1,7 @@
+module.exports = {
+  RPC_URL: "https://t3rn-b2n.blockpi.network/v1/rpc/public",
+  CHAIN_ID: 334,
+  SYMBOL: "BRN",
+  TX_EXPLORER: "https://b2n.explorer.caldera.xyz/tx/",
+  ADDRESS_EXPLORER: "https://b2n.explorer.caldera.xyz/address/",
+};
