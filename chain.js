1602b1743938e11be4e093e1b0fdf57ed6d2695c
hanifdamarjati20 <hanif.damarjati20@gmail.com>
2025-05-01 23:55:49 +0700

    update to multichain

--- chain.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  RPC_URL: "https://t3rn-b2n.blockpi.network/v1/rpc/public",
-  CHAIN_ID: 334,
-  SYMBOL: "BRN",
-  TX_EXPLORER: "https://b2n.explorer.caldera.xyz/tx/",
-  ADDRESS_EXPLORER: "https://b2n.explorer.caldera.xyz/address/",
-};
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { ethers } = require('ethers');
-const chain = require(path.join(__dirname, 'chain.js'));
+const chain = require(path.join(__dirname, 'chainmulti.js'));
 
 const walletsPath = path.join(__dirname, 'wallets.json');
 let wallets;
@@ -12,16 +12,39 @@ try {
   process.exit(1);
 }
 
-const provider = new ethers.providers.JsonRpcProvider(chain.RPC_URL, chain.CHAIN_ID);
+const provider0 = new ethers.providers.JsonRpcProvider(chain.RPC_URL0, chain.CHAIN_ID0);
+const provider1 = new ethers.providers.JsonRpcProvider(chain.RPC_URL1, chain.CHAIN_ID1);
+const provider2 = new ethers.providers.JsonRpcProvider(chain.RPC_URL2, chain.CHAIN_ID2);
+const provider3 = new ethers.providers.JsonRpcProvider(chain.RPC_URL3, chain.CHAIN_ID3);
+const provider4 = new ethers.providers.JsonRpcProvider(chain.RPC_URL4, chain.CHAIN_ID4);
+const provider5 = new ethers.providers.JsonRpcProvider(chain.RPC_URL5, chain.CHAIN_ID5);
+
+// Array of providers and their corresponding chain symbols
+const providers = [
+  { provider: provider0, symbol: chain.SYMBOL0 },
+  { provider: provider1, symbol: chain.SYMBOL1 },
+  { provider: provider2, symbol: chain.SYMBOL2 },
+  { provider: provider3, symbol: chain.SYMBOL3 },
+  { provider: provider4, symbol: chain.SYMBOL4 },
+  { provider: provider5, symbol: chain.SYMBOL5 },
+];
 
 async function checkBalances() {
   for (const wallet of wallets) {
-    try {
-      const balance = await provider.getBalance(wallet.address);
-      const formattedBalance = ethers.utils.formatEther(balance);
-      console.log(`💰 Wallet - [${wallet.address}] has a balance of [${formattedBalance} ${chain.SYMBOL}]`);
-    } catch (error) {
-      console.error(`Error getting balance for wallet ${wallet.address}:`, error);
+    console.log(`Checking balances for wallet: ${wallet.address}`);
+    for (const { provider, symbol } of providers) {
+      try {
+        const balance = await provider.getBalance(wallet.address);
+        const formattedBalance = Number(ethers.utils.formatEther(balance)).toFixed(2);
+        console.log(
+          `💰 Wallet - [${wallet.address}] has a balance of [${formattedBalance} ${symbol}]`
+        );
+      } catch (error) {
+        console.error(
+          `Error getting balance for wallet ${wallet.address} on chain ${provider.network.chainId}:`,
+          error
+        );
+      }
     }
   }
 }
